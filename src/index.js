5ce5963e9fa9f9e8689a1b5e1085653f1becb9e8
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-28 20:26:47 -0500

    Added documentation.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+/******************************************************************************
+ *         Name: index.js
+ *       Author: Chad Chapman
+ * Date Created: December 28, 2022
+ *  Description: Functions that support implementation of professional 
+ *               portfolio website.
+******************************************************************************/
+
+/******************************************************************************
+ * IMPORTS
+ *****************************************************************************/
 import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script.
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import './css/styles.css';
