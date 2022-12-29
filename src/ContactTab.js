aa6573d1cffd8fe633cbffa86d5293087dfb1b81
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-28 23:17:56 -0500

    Created class structure for other pages.

--- src/ContactTab.js
+++ src/ContactTab.js
@@ -5,3 +5,29 @@
  *  Description: Contains class that is responsible for rendering the contact  
  *               tab page's contents.
 ******************************************************************************/
+
+/******************************************************************************
+ * IMPORTS
+ *****************************************************************************/
+import { Page } from "./Page.js";
+
+/**
+ * 
+ */
+export class ContactTab extends Page {
+    /**
+     * Default constuctor.
+     */
+    constructor() {
+        super();
+    }
+
+    initializeMain() {
+        const mainContainer = document.createElement('div');
+        mainContainer.setAttribute('id', 'main');
+        mainContainer.classList.add('main');
+        mainContainer.textContent = "Main - Contact Tab";
+
+        return mainContainer;
+    }
+}
