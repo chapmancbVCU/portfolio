1ca0887461f7170093b712ea8a2875a59bc234c7
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-28 22:55:13 -0500

    Created classes with test output.

--- src/ResumeTab.js
+++ src/ResumeTab.js
@@ -5,3 +5,29 @@
  *  Description: Contains class that is responsible for rendering the resume 
  *               tab page's contents.
 ******************************************************************************/
+
+/******************************************************************************
+ * IMPORTS
+ *****************************************************************************/
+import { Page } from "./Page.js";
+
+/**
+ * 
+ */
+export class ResumeTab extends Page {
+    /**
+     * Default constuctor.
+     */
+    constructor() {
+        super();
+    }
+
+    initializeMain() {
+        const mainContainer = document.createElement('div');
+        mainContainer.setAttribute('id', 'main');
+        mainContainer.classList.add('main');
+        mainContainer.textContent = "Main - Resume Tab";
+
+        return mainContainer;
+    }
+}
