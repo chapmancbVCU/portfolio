4c85450f2a83d38a00f5be86b4ae461317747bfa
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-28 20:42:57 -0500

    Added divs for header, main, and footer.

--- src/Page.js
+++ src/Page.js
@@ -9,5 +9,38 @@
  * 
  */
 export class Page {
+    /**
+     * Default constructor
+     */
+    constructor() {
+        this.container = document.querySelector('#content');
+        this.container.appendChild(this.initializeHeader());
+        this.container.appendChild(this.initializeMain());
+        this.container.appendChild(this.initializeFooter());
+    }
 
+    initializeFooter() {
+        const footerContainer = document.createElement('div');
+        footerContainer.textContent = "Footer";
+
+        return footerContainer;
+    }
+
+    /**
+     * 
+     * @returns 
+     */
+    initializeHeader() {
+        const headerContainer = document.createElement('div');
+        headerContainer.textContent = "Header";
+
+        return headerContainer;
+    }
+
+    initializeMain() {
+        const mainContainer = document.createElement('div');
+        mainContainer.textContent = "Main";
+
+        return mainContainer;
+    }
 }
