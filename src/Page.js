578eba38aa04038e93443c823d02085f30022efc
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-28 20:33:52 -0500

    Initial commit

---
+++ src/Page.js
@@ -0,0 +1,13 @@
+/******************************************************************************
+ *         Name: Page.js
+ *       Author: Chad Chapman
+ * Date Created: December 28, 2022
+ *  Description: Contains class that is responsible for rendering page 
+ *               elements.
+******************************************************************************/
+/**
+ * 
+ */
+export class Page {
+
+}
