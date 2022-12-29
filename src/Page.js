ec72f53b704503a21355abfd3edd995fc733b11e
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-28 21:30:09 -0500

    Setup list as tabs.

--- src/Page.js
+++ src/Page.js
@@ -21,6 +21,7 @@ export class Page {
 
     initializeFooter() {
         const footerContainer = document.createElement('div');
+        footerContainer.classList.add('footer')
         footerContainer.textContent = "Footer";
 
         return footerContainer;
@@ -32,13 +33,54 @@ export class Page {
      */
     initializeHeader() {
         const headerContainer = document.createElement('div');
-        headerContainer.textContent = "Header";
+        headerContainer.classList.add('header');
+        const headerImage = document.createElement('div');
+        headerImage.textContent = 'Image';
+        headerContainer.appendChild(headerImage);
+
+        const menuContainer = document.createElement('div');
+        const menuList = document.createElement('ul');
+        menuList.classList.add('menu-list');
+
+        const homeTab = document.createElement('li');
+        homeTab.setAttribute('id', 'home-tab');
+        homeTab.classList.add('page-tab');
+        homeTab.textContent = 'Home';
+        menuList.appendChild(homeTab);
+
+        const resumeTab = document.createElement('li');
+        resumeTab.setAttribute('id', 'resume-tab');
+        resumeTab.classList.add('page-tab');
+        resumeTab.textContent = 'Resume';
+        menuList.appendChild(resumeTab);
+
+        const projectsTab = document.createElement('li');
+        projectsTab.setAttribute('id', 'projects-tab');
+        projectsTab.classList.add('page-tab');
+        projectsTab.textContent = 'Projects';
+        menuList.appendChild(projectsTab);
+
+        const aboutTab = document.createElement('li');
+        aboutTab.setAttribute('id', 'about-tab');
+        aboutTab.classList.add('page-tab');
+        aboutTab.textContent = "About";
+        menuList.appendChild(aboutTab);
+
+        const contactTab = document.createElement('li');
+        contactTab.setAttribute('id', 'contact-tab');
+        contactTab.classList.add('page-tab');
+        contactTab.textContent = 'Contact';
+        menuList.appendChild(contactTab);
+        
+        menuContainer.appendChild(menuList);
+        headerContainer.appendChild(menuContainer);
 
         return headerContainer;
     }
 
     initializeMain() {
         const mainContainer = document.createElement('div');
+        mainContainer.classList.add('main');
         mainContainer.textContent = "Main";
 
         return mainContainer;
