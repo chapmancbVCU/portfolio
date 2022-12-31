4680206c26ef6dcbef852e5eb3c2da9e82128529
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-30 20:25:38 -0500

    Added fuction for first section of page.

--- src/ResumeTab.js
+++ src/ResumeTab.js
@@ -34,8 +34,40 @@ export class ResumeTab extends Page {
         const mainContainer = document.createElement('div');
         mainContainer.setAttribute('id', 'main');
         mainContainer.classList.add('main');
-        mainContainer.textContent = "Main - Resume Tab";
+        
+        const pagetitle = document.createElement('h3');
+        pagetitle.textContent = "Resume";
+        mainContainer.appendChild(pagetitle);
 
+        
+        mainContainer.appendChild(this.initializeWelcomeContainer());
+
+        mainContainer.appendChild(this.initializeResumeContent());
         return mainContainer;
+    }
+
+
+
+
+
+    initializeResumeContent() {
+        const resumeContainer = document.createElement('div');
+        resumeContainer.classList.add('content-container');
+
+        return resumeContainer;
+    }
+
+    initializeWelcomeContainer() {
+        const welcomeContainer = document.createElement('div');
+        welcomeContainer.classList.add('content-container');
+
+        const welcomeMessage1 = document.createElement('p');
+        welcomeMessage1.textContent = "Below is a complete resume that includes skills that I'm not only very proficient in but also languages, tools, and technology I have been exposed to over the years.";
+        welcomeContainer.appendChild(welcomeMessage1);
+
+        const welcomeMessage2 = document.createElement('p');
+        welcomeMessage2.textContent = "Reach out to me and I can provide a job tailored resume for you.";
+        welcomeContainer.appendChild(welcomeMessage2);
+        return welcomeContainer;
     }
 }
