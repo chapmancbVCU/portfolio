9db1553b72ca6c11a7eead0df44cd944bfc99f89
Chad Chapman <chad.chapman2010@gmail.com>
2022-12-31 20:17:17 -0500

    Added resume content and necessary styling.

--- src/ResumeTab.js
+++ src/ResumeTab.js
@@ -24,7 +24,7 @@ export class ResumeTab extends Page {
 
     /**
      * Renders the main section of the webpage.
-     * @returns The HTMLDivElement that contains the main section.
+     * @returns HTMLDivElement The div that contains the main section.
      */
     initializeMain() {
         const mainContainer = document.createElement('div');
@@ -44,64 +44,40 @@ export class ResumeTab extends Page {
 
     /**
      * Renders the resume section of the webpage.
-     * @returns The HTMLDivElement that contains the resume section.
+     * @returns HTMLDivElement The div that contains the resume section.
      */
     initializeResumeContent() {
         const resumeContainer = document.createElement('div');
         resumeContainer.classList.add('content-container');
 
-        const name = document.createElement('h2');
-        name.classList.add('name');
-        name.textContent = 'Chad Chapman';
-        resumeContainer.appendChild(name);
+        resumeContainer.appendChild(this.renderName());
+        resumeContainer.appendChild(this.renderEmail());
+        resumeContainer.appendChild(this.renderPhoneNumber());
 
-        const email = document.createElement('p');
-        email.classList.add('resume-heading');
-        email.textContent = 'cbchapman@alumni.vcu.edu';
-        resumeContainer.appendChild(email);
+        resumeContainer.appendChild(this.renderBorder());
 
-        const phoneNumber = document.createElement('p');
-        phoneNumber.classList.add('resume-heading');
-        phoneNumber.textContent = '804-426-0891';
-        resumeContainer.appendChild(phoneNumber);
+        resumeContainer.appendChild(this.renderJobTitle());
+        resumeContainer.appendChild(this.renderGitHubLink());
+        resumeContainer.appendChild(this.renderLinkedInLink());
 
-        const border = document.createElement('div');
-        border.classList.add('resume-border');
-        resumeContainer.appendChild(border);
-
-        const jobTitle = document.createElement('div');
-        jobTitle.classList.add('resume-job-title');
-        jobTitle.textContent = 'Computer Scientist';
-        resumeContainer.appendChild(jobTitle);
-
-        const githubLinkContainer = document.createElement('div');
-        githubLinkContainer.classList.add('resume-link');
-        githubLinkContainer.textContent = 'GitHub: ';
-        const githubLink = document.createElement('a');
-        githubLink.textContent = 'https://github.com/chapmancbVCU';
-        githubLink.setAttribute('href', 'https://github.com/chapmancbVCU');
-        githubLinkContainer.appendChild(githubLink);
-        resumeContainer.appendChild(githubLinkContainer);
-
-        const linkedInLinkContainer = document.createElement('div');
-        linkedInLinkContainer.classList.add('resume-link');
-        linkedInLinkContainer.textContent = 'LinkedIn: ';
-        const linkedInLink = document.createElement('a');
-        linkedInLink.textContent = 'https://www.linkedin.com/in/chadchapman2010/';
-        linkedInLink.setAttribute('href', 'https://www.linkedin.com/in/chadchapman2010/');
-        linkedInLinkContainer.appendChild(linkedInLink);
-        resumeContainer.appendChild(linkedInLinkContainer);
-
-        resumeContainer.appendChild(this.renderSectionBorder('Summary of Technical Skills'));
+        resumeContainer.appendChild(this.renderSectionBorder(
+            'Summary of Technical Skills'));
 
         resumeContainer.appendChild(this.renderSkillsList());
 
+        resumeContainer.appendChild(this.renderSectionBorder(
+            'Professional Experience'));
+
+        resumeContainer.appendChild(this.renderProfessionalExperience());
+
+        resumeContainer.appendChild(this.renderSectionBorder('Education'));
+        
         return resumeContainer;
     }
 
     /**
      * Renders the welcome section of the resume tab.
-     * @returns The HTMLDivElement that contains the welcome section of the 
+     * @returns HTMLDivElement The div that contains the welcome section of the 
      * resume tab.
      */
     initializeWelcomeContainer() {
@@ -117,9 +93,39 @@ export class ResumeTab extends Page {
         welcomeMessage2.classList.add('paragraph-content');
         welcomeMessage2.textContent = "Reach out to me and I can provide a job tailored resume for you.";
         welcomeContainer.appendChild(welcomeMessage2);
+        
         return welcomeContainer;
     }
 
+    /**
+     * Renders the border near the top of the resume.
+     * @returns HTMLDivElement with border styling that acts as a border
+     * at the top of the resume.
+     */
+    renderBorder() {
+        const border = document.createElement('div');
+        border.classList.add('resume-border');
+
+        return border;
+    }
+
+    /**
+     * Renders the E-mail.
+     * @returns HTMLParagraphElement The resume E-mail.
+     */
+    renderEmail() {
+        const email = document.createElement('p');
+        email.classList.add('resume-heading');
+        email.textContent = 'cbchapman@alumni.vcu.edu';
+
+        return email;
+    }
+
+    /**
+     * Renders the list of environments and tools as a list element.
+     * @returns HTMLLIElement The list of environments and tools for the
+     * resume.
+     */
     renderEnvironmentsAndToolsList() {
         const environmentAndToolsLi = document.createElement('li');
         environmentAndToolsLi.classList.add('skills-list-item');
@@ -133,6 +139,60 @@ export class ResumeTab extends Page {
         environmentAndToolsLi.appendChild(environmentList);
 
         return environmentAndToolsLi
+    }
+
+    /**
+     * Renders the link for my GitHub profile.
+     * @returns HTMLDivElement The 
+     */
+    renderGitHubLink() {
+        const githubLinkContainer = document.createElement('div');
+        githubLinkContainer.classList.add('resume-link');
+        githubLinkContainer.textContent = 'GitHub: ';
+        const githubLink = document.createElement('a');
+        githubLink.textContent = 'https://github.com/chapmancbVCU';
+        githubLink.setAttribute('href', 'https://github.com/chapmancbVCU');
+        githubLinkContainer.appendChild(githubLink);
+
+        return githubLinkContainer;
+    }
+
+    /**
+     * Renders the job title for my resume.
+     * @returns HTMLDivElement The div element that contains the job title for 
+     * the resume.
+     */
+    renderJobTitle() {
+        const jobTitle = document.createElement('div');
+        jobTitle.classList.add('resume-job-title');
+        jobTitle.textContent = 'Computer Scientist';
+
+        return jobTitle;
+    }
+
+    renderFacilityContainer(boldText, regularText, location, years) {
+        const facilityContainer = document.createElement('div');
+        facilityContainer.classList.add('facility-container');
+    
+        const left = document.createElement('div');
+        const boldTextContent = document.createElement('strong');
+        boldTextContent.textContent = boldText;
+        left.appendChild(boldTextContent);
+        const regularTextContent = document.createElement('p');
+        regularTextContent.textContent = regularText;
+        left.appendChild(regularTextContent);
+        facilityContainer.appendChild(left);
+
+        const right = document.createElement('div');
+        right.classList.add('facility-container-right');
+        const locationContent = document.createElement('p');
+        locationContent.textContent = location;
+        right.appendChild(locationContent);
+        const yearsContent = document.createElement('p');
+        yearsContent.textContent = years;
+        right.appendChild(yearsContent);
+        facilityContainer.appendChild(right);
+        return facilityContainer;
     }
 
     renderLanguagesAndLibrariesList() {
@@ -150,6 +210,34 @@ export class ResumeTab extends Page {
         return languagesAndLibrariesLi;
     }
     
+    renderLinkedInLink() {
+        const linkedInLinkContainer = document.createElement('div');
+        linkedInLinkContainer.classList.add('resume-link');
+        linkedInLinkContainer.textContent = 'LinkedIn: ';
+        const linkedInLink = document.createElement('a');
+        linkedInLink.textContent = 'https://www.linkedin.com/in/chadchapman2010/';
+        linkedInLink.setAttribute('href', 'https://www.linkedin.com/in/chadchapman2010/');
+        linkedInLinkContainer.appendChild(linkedInLink);
+
+        return linkedInLinkContainer;
+    }
+
+    renderName() {
+        const name = document.createElement('h2');
+        name.classList.add('name');
+        name.textContent = 'Chad Chapman';
+
+        return name;
+    }
+
+    renderPhoneNumber() {
+        const phoneNumber = document.createElement('p');
+        phoneNumber.classList.add('resume-heading');
+        phoneNumber.textContent = '804-426-0891';
+
+        return phoneNumber;
+    }
+
     renderPlatformsList() {
         const platformsLi = document.createElement('li');
         platformsLi.classList.add('skills-list-item');
@@ -165,10 +253,72 @@ export class ResumeTab extends Page {
         return platformsLi;
     }
 
+    renderProfessionalExperience() {
+        const professionalExperienceContainer = document.createElement('div');
+        professionalExperienceContainer.classList.add(
+                'professional-experience-container');
+
+        professionalExperienceContainer.appendChild(this.renderFacilityContainer(
+                'Science Systems and Applications, Inc.',
+                'Computer Scientist',
+                'Hampton, VA',
+                '08-2012 - Present'));
+
+        const experienceList = document.createElement('ul');
+        experienceList.classList.add('experience-list');
+
+        const lineOne = document.createElement('li');
+        lineOne.textContent = 'Provided simulation services for our civil servant customers at NASA Langley Research Center in the role of a Systems Engineer for the Air Traffic Operations Laboratory (ATOL).';
+        experienceList.appendChild(lineOne);
+
+        const lineTwo = document.createElement('li');
+        lineTwo.textContent = 'Reduced our customer’s operational costs by designing and deploying a video streaming solution using a Nginx server that supports live streaming and Video on Demand (VOD) services.';
+        experienceList.appendChild(lineTwo);
+        
+        const lineThree = document.createElement('li');
+        lineThree.textContent = 'Developed the website front end for the video streaming solution which references a database that stores searchable records of videos available to the VOD service.';
+        experienceList.appendChild(lineThree);
+
+        const lineFour = document.createElement('li');
+        lineFour.textContent = 'Created a user guide for how to operate the services provided by the Nginx server.';
+        experienceList.appendChild(lineFour);
+
+        const lineFive = document.createElement('li');
+        lineFive.textContent = 'Collaborated with our System Administrators to perform upgrades and conduct testing of the ATOL facility’s video distribution system.';
+        experienceList.appendChild(lineFive);
+
+        const lineSix = document.createElement('li');
+        lineSix.textContent = 'Automated the deployment of several generations of our configuration baseline with Ansible to hardware assets in our customer’s test and verification laboratory and production environments';
+        experienceList.appendChild(lineSix);
+
+        const lineSeven = document.createElement('li');
+        lineSeven.textContent = 'Implemented a Python script that reports statistics of error rates from large datasets.';
+        experienceList.appendChild(lineSeven);
+
+        const lineEight = document.createElement('li');
+        lineEight.textContent = 'Ensured the secured delivery of experiment data to researchers and members of the development team following the CIA (Confidentiality, Integrity, Availability) of IT Security industry standard.';
+        experienceList.appendChild(lineEight);
+
+        const lineNine = document.createElement('li');
+        lineEight.textContent = 'Developed a maintenance tool in Java that generates a report of outdated simulation software builds on systems throughout the production environment.';
+        experienceList.appendChild(lineNine);
+
+        professionalExperienceContainer.appendChild(experienceList);
+
+        return professionalExperienceContainer;
+    }
+
+    /**
+     * Renders the section borders of the resume.
+     * @param {string} textContent The title that is rendered as the border's 
+     * text.
+     * @returns The HTMLDivElement that is used as a section border.
+     */
     renderSectionBorder(textContent) {
         const sectionBorder = document.createElement('div');
         sectionBorder.classList.add('resume-section-border');
         sectionBorder.textContent = textContent;
+        
         return sectionBorder;
     }
 
