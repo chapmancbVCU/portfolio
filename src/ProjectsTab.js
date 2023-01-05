5bc37811c07a975b7a1b6d73f8dde0e18ab5d7c3
Chad Chapman <chad.chapman2010@gmail.com>
2023-01-04 20:37:07 -0500

    Began to implement template for projects and associated styling.

--- src/ProjectsTab.js
+++ src/ProjectsTab.js
@@ -10,7 +10,7 @@
  * IMPORTS
  *****************************************************************************/
 import { Page } from "./Page.js";
-
+import CircuitImageAnn from "./images/circuits-ann.jpg";
 /**
  * Class whose functions renders components of the Projects Tab.
  */
@@ -37,6 +37,8 @@ export class ProjectsTab extends Page {
         mainContainer.appendChild(pagetitle);
 
         mainContainer.appendChild(this.initializeWelcomeContainer());
+
+        mainContainer.appendChild(this.renderProject('test', 'test', 'test', CircuitImageAnn));
         return mainContainer;
     }
 
@@ -51,9 +53,51 @@ export class ProjectsTab extends Page {
 
         const welcomeMessage1 = document.createElement('p');
         welcomeMessage1.classList.add('paragraph-content');
-        welcomeMessage1.textContent = "Below is a of projects I have completed in my leisure time and while completing my studies at VCU.  Here you will find a variety of pojects from websites to hardware designs.";
+        welcomeMessage1.textContent = "Below is a collection of projects I have completed in my leisure time and while completing my studies at VCU.  Here you will find a variety of pojects from websites to hardware designs.";
         welcomeContainer.appendChild(welcomeMessage1);
 
         return welcomeContainer;
+    }
+
+    renderProject(title, builtWith, description, image) {
+        const projectContainer = document.createElement('div');
+        projectContainer.classList.add('content-container');
+
+        const projectTitle = document.createElement('h3');
+        projectTitle.classList.add('project-title');
+        projectTitle.textContent = title;
+        projectContainer.appendChild(projectTitle);
+
+        const projectInfo = document.createElement('div');
+        projectInfo.classList.add('project-info');
+
+        const projectImage = new Image();
+        projectImage.classList.add('project-image');
+        projectImage.src = image;
+        projectInfo.appendChild(projectImage);
+
+        const projectInfoRight = document.createElement('div');
+        projectInfoRight.classList.add('project-info-right');
+
+        const builtWithContainer = document.createElement('div')
+        builtWithContainer.classList.add('built-with-container');
+
+        const builtWithLabel = document.createElement('strong');
+        builtWithLabel.textContent = 'Built with:';
+        builtWithContainer.appendChild(builtWithLabel);
+
+        const builtWithList = document.createElement('p');
+        builtWithList.textContent = builtWith;
+        builtWithContainer.appendChild(builtWithList);
+        projectInfoRight.appendChild(builtWithContainer);
+
+        const projectDescription = document.createElement('p');
+        projectDescription.textContent = description;
+        projectInfoRight.appendChild(projectDescription);
+
+        projectInfo.appendChild(projectInfoRight);
+        projectContainer.appendChild(projectInfo);
+        
+        return projectContainer;
     }
 }
