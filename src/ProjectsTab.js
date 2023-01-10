2c62deeaec77b3013a0f54eeae93514b526bc94a
Chad Chapman <chad.chapman2010@gmail.com>
2023-01-10 18:54:16 -0500

    Fixed link for electronic combination lock writup.

--- src/ProjectsTab.js
+++ src/ProjectsTab.js
@@ -106,7 +106,7 @@ export class ProjectsTab extends Page {
         builtWith = 'VHDL Hardware Description Language with Xilinx ISE';
         description = 'An implementation of an electronic combination lock.  After the code was written we synthesized the design to a Field Programmable Gate Array FPGA and demonstrated it to the graduate teaching assistant.  The design required the use of finite state machines to determine if the input is correct.  Depending on the input, a set of 4 LEDs will blink in a certain pattern to indicate if the entered combination is correct.  The live link will take you to the writeup for this assignment.';
         codeLink = `location.href='https://github.com/chapmancbVCU/EGRE-427-Electronic-Combination-Lock'`;
-        liveLink = `location.href='https://github.com/chapmancbVCU/EGRE-427-Electronic-Combination-Lock/blob/main/HW2%20Report.pdf'`;
+        liveLink = `location.href='https://docs.google.com/viewer?url=https%3A%2F%2Fgithub.com%2FchapmancbVCU%2FEGRE-427-Electronic-Combination-Lock%2Fraw%2Fmain%2FHW2%2520Report.pdf'`;
         mainContainer.appendChild(this.renderProject(
             title, builtWith, description, 
             CombinationLock, codeLink, liveLink));
