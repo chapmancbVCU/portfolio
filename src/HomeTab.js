d2e1495f95070b9f88d990240bde1a6d1db6ba4d
Chad Chapman <chad.chapman2010@gmail.com>
2023-03-18 13:35:36 -0400

    Improved content of home tab.

--- src/HomeTab.js
+++ src/HomeTab.js
@@ -51,28 +51,24 @@ export class HomeTab extends Page {
 
         const welcomeMessage1 = document.createElement('p');
         welcomeMessage1.classList.add('paragraph-content');
-        welcomeMessage1.textContent = 'My name is Chad Chapman.  I’m a Systems Engineer with over 10 years of professional experience working for my customer at NASA Langley Research Center in Hampton, VA.  I work at a facility known as the Air Traffic Operations Laboratory (ATOL).  The ATOL is a world class facility known for supporting research with the goal of introducing new technology and improving the operations of the National Airspace (NAS).'
+        welcomeMessage1.textContent = 'Welcome to my portfolio webpage! My name is Chad Chapman, and I\'m a Systems Engineer with over 10 years of professional experience working at NASA Langley Research Center in Hampton, VA. Specifically, I work at the Air Traffic Operations Laboratory (ATOL), a world-class facility known for supporting research that aims to introduce new technology and improve the operations of the National Airspace (NAS).'
         welcomeContainer.appendChild(welcomeMessage1);
 
         const welcomeMessage2 = document.createElement('p');
         welcomeMessage2.classList.add('paragraph-content');
-        welcomeMessage2.textContent = 'While not supporting research I assist in improving the ATOL’s infrastructure and services.  For example, we have spent a lot of effort improving our ability to distribute video throughout the lab over the past 3 years.  My major contribution is the design, implementation, and rollout of a video streaming appliance.  This appliance consists of a Nginx server compiled with the appropriate modules and a website front end so our team can interact with it.  A database is used to store information about videos stored on the server so users can search for content and finally begin playback.';
+        welcomeMessage2.textContent = 'Apart from my work supporting research at ATOL, I also assist in improving the lab\'s infrastructure and services. One of my significant contributions is the design, implementation, and rollout of a video streaming appliance, which has greatly enhanced our ability to distribute video throughout the lab. This project has piqued my interest in web development, which I\'ve been exploring extensively in my spare time, using online resources such as freeCodeCamp and The Odin Project.';
         welcomeContainer.appendChild(welcomeMessage2);
 
         const welcomeMessage3 = document.createElement('p');
         welcomeMessage3.classList.add('paragraph-content');
-        welcomeMessage3.textContent = 'After working on this project for some time I have developed an interest in web development.  I am thankful for a particular former manager at work who encouraged me to grow as a professional.  So, I decided to spend a good portion of my time outside of work to learn web development and apply what I have learned in the workplace.  Most of this skill development has been acquired through the utilization of free online resources such as freeCodeCamp and The Odin Project.';
+        welcomeMessage3.textContent = 'Over time, my passion for web development has grown, and I\'ve been applying my skills to real-world projects. I\'m excited to explore full-time opportunities in this field and take my career to the next level. Please feel free to check out the rest of my portfolio website and projects. If you have any questions, I\'d be happy to answer them. You can find my contact information and GitHub profile links below.';
         welcomeContainer.appendChild(welcomeMessage3);
 
         const welcomeMessage4 = document.createElement('p');
         welcomeMessage4.classList.add('paragraph-content');
-        welcomeMessage4.textContent = 'After spending some time learning and applying acquired skills in a professional setting, I have decided that full time web development could be a potential career move.  Please feel free to check out the rest of my portfolio website and projects.  If you have any questions don’t hesitate to ask.';
+        welcomeMessage4.textContent = 'Thank you for visiting my portfolio!';
         welcomeContainer.appendChild(welcomeMessage4);
 
-        const welcomeMessage5 = document.createElement('p');
-        welcomeMessage5.classList.add('paragraph-content');
-        welcomeMessage5.textContent = 'Links for contacting me and my GitHub profile can be found below.';
-        welcomeContainer.appendChild(welcomeMessage5);
         return welcomeContainer;
     }
 }
